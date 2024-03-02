4bb6c0f9c24d686f29e8c598e4febfcb87d0e5b3
andgeral <geraldinebecerra93@gmail.com>
2024-03-02 00:24:38 -0300

    se quitan las validaciones

--- src/impuestos.js
+++ src/impuestos.js
@@ -1,24 +1,8 @@
 //se crea la clase impuestos
 class Impuestos {
   constructor(montoBrutoAnual, deducciones) {
-    // se valida que el valor ingresado sea un numero y que sea mayor o igual a 0
-    if (
-      !isNaN(montoBrutoAnual) &&
-      !isNaN(deducciones) &&
-      montoBrutoAnual >= 0 &&
-      deducciones >= 0
-    ) {
-      this._montoBrutoAnual = montoBrutoAnual;
-      this._deducciones = deducciones;
-    } else {
-      console.log(
-        "El monto bruto anual y deducciones deben ser valores numéricos positivos."
-      );
-    }
-    //se valida si el monto de las deducciones es mayor que monto bruto arroge un  mensaje
-    if (deducciones > montoBrutoAnual) {
-      console.log("Queda exonerado de Impuestos Anuales");
-    }
+    this._montoBrutoAnual = montoBrutoAnual;
+    this._deducciones = deducciones;
   }
 
   get montoBrutoAnual() {
